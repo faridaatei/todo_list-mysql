b8708d958236f78fe4aff3597d3d75f200b01782
Farida Atei <faridaabdalla0@gmail.com>
2018-10-27 13:14:29 +0300

    handle 404 error

--- app.py
+++ app.py
@@ -1,41 +1,49 @@
-#!flask/Source/python
-import mysql.connector
-from  mysql.connector import errorcode
-from flask import Flask,jsonify 
- 
+#!flask/source/pythonst
 
+from flask import Flask,jsonify, request 
+from config import Config
 
-
-def mysql_conn():
-
-
-    try:
-      connection = mysql.connector.connect(
-        host = 'localhost',
-        user ='root',
-        password='nana',
-        database = "todo_api")         
-    except mysql.connector.Error as err:
-      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-      elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-      else:
-        print(err)
-    return connection
-app =app = Flask('app')
+config = Config ()
+cursor = config.get_cursor()
+app = Flask ('app')
 
 @app.route('/')
-def index():
-    return 'Hello world'
+def index ():
+  return 'Hello World'
+  @app.errorhandler(404)
+  def page_not_found(error):
+    result = {
+    "statusCode": 404,
+    "status":"failed",
+    "message": "Not found"
+    }
+    return jsonify(result)
 
-@app.route('/user')
-def get_users():
-  conn =mysql_conn()
-  cursor =conn.cursor()
+
+@app.route('/user', methods=['GET'])
+def get_users ():
+  
   cursor.execute("SELECT * FROM user")
   result = cursor.fetchall()
+
   return jsonify(result)
 
-if __name__== '__main__':
+@app.route('/register', methods=['POST'])
+def register_user ():
+  result = request.get_json()
+
+  #Get data from the request object
+  name = result['name']
+  email = result['email']
+  password = result['password']
+
+  #insert data to the table
+  query = "INSERT INTO user VALUES (%s, %s, %s, %s, NOW())"
+  values = (3, name, email, password)
+  cursor.execute(query, values)
+
+  return jsonify(result)
+
+
+if __name__ == '__main__':
   app.run(debug=True)
